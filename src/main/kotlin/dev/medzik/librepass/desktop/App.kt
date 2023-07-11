ff4aabb5adf17eacfb2f3c09e1429c435fb6ebc4
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-07-11 23:18:28 +0200

    feat: added app icon

--- src/main/kotlin/dev/medzik/librepass/desktop/App.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/App.kt
@@ -5,6 +5,7 @@ import dev.medzik.librepass.desktop.state.StateManager
 import dev.medzik.librepass.desktop.style.StyleManager
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.image.Image
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
 import java.util.*
@@ -13,8 +14,9 @@ class App : Application() {
     override fun start(stage: Stage) {
         val scene = Scene(Pane(), 640.0, 480.0)
 
-        stage.title = "LibrePass"
+        stage.title = "LibrePass"t co
         stage.scene = scene
+        stage.icons.add(Image(App::class.java.getResourceAsStream("/img/logo.png")))
 
         val resources: ResourceBundle = ResourceBundle.getBundle("/locales/locale", Locale.getDefault())
 
