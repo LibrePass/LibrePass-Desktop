4515de935f222a2e52c33e83d8d35ab94b2a6dfd
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-08-09 21:46:38 +0200

    feat: added local database sync

--- src/main/kotlin/dev/medzik/librepass/desktop/gui/dashboard/DashboardController.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/gui/dashboard/DashboardController.kt
@@ -1,11 +1,16 @@
 package dev.medzik.librepass.desktop.gui.dashboard
 
+import dev.medzik.libcrypto.EncryptException
 import dev.medzik.librepass.client.api.CipherClient
+import dev.medzik.librepass.desktop.config.Config
 import dev.medzik.librepass.desktop.config.Credentials
 import dev.medzik.librepass.desktop.config.DataStoreUserSecrets
 import dev.medzik.librepass.desktop.gui.Controller
 import dev.medzik.librepass.desktop.gui.components.CipherListItem
 import dev.medzik.librepass.types.cipher.Cipher
+import dev.medzik.librepass.types.cipher.CipherType
+import dev.medzik.librepass.types.cipher.EncryptedCipher
+import dev.medzik.librepass.types.cipher.data.CipherLoginData
 import javafx.application.Platform
 import javafx.collections.FXCollections
 import javafx.fxml.FXML
@@ -20,14 +25,40 @@ class DashboardController : Controller() {
     private lateinit var cipherClient: CipherClient
 
     @FXML
-    lateinit var cipherList: ListView<CipherListItem>
+    lateinit var cipherList: ListView<Cipher>
 
-    private val list = FXCollections.observableArrayList<CipherListItem>()
+    private val list = FXCollections.observableArrayList<Cipher>()
 
     @FXML
     private fun initialize() {
         cipherList.items = list
+        cipherList.setCellFactory { CipherListItem() }
     }
+
+    // get ciphers from local repository and update UI
+    fun updateLocalCiphers() {
+        val ciphers = Config.readObject<List<EncryptedCipher>>("ciphers")
+
+        val decryptedCiphers = ciphers.map {
+            try {
+                Cipher(it, userSecrets.secretKey)
+            } catch (e: EncryptException) {
+                Cipher(
+                    id = it.id,
+                    owner = it.owner,
+                    type = CipherType.Login,
+                    loginData = CipherLoginData(
+                        name = "Encryption error"
+                    )
+                )
+            }
+        }
+
+        // sort ciphers by name and update UI
+        decryptedCiphers.sortedBy { it.loginData!!.name }.forEach { list.add(it) }
+    }
+
+    fun updateCiphers() {}
 
     override fun onStart() {
         super.onStart()
@@ -40,8 +71,7 @@ class DashboardController : Controller() {
             for (cipher in ciphersResponse) {
                 val cipherElement = Cipher(cipher, userSecrets.secretKey)
 
-                val item = CipherListItem(cipherElement)
-                Platform.runLater { list.add(item) }
+                Platform.runLater { list.add(cipherElement) }
             }
         }
     }
--- src/main/kotlin/dev/medzik/librepass/desktop/utils/Fxml.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/utils/Fxml.kt
@@ -4,7 +4,10 @@ import dev.medzik.librepass.desktop.gui.components.CipherListItem
 import javafx.fxml.FXMLLoader
 
 object Fxml {
-    fun load(path: String, controller: Any, rootNode: Any = controller) {
+
+    fun getLoader(path: String) = FXMLLoader(CipherListItem::class.java.getResource(path))
+
+    fun load(path: String, controller: Any?, rootNode: Any? = controller) {
         val loader = FXMLLoader(CipherListItem::class.java.getResource(path))
         loader.setController(controller)
         loader.setRoot(rootNode)
