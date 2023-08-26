022c16844e5b2be31acf34f18c075ef31a1ad089
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-08-26 22:18:38 +0200

    chore: moved styles to one directory

--- src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
@@ -4,8 +4,8 @@ enum class Style(
     val hook: () -> Unit,
     vararg val styles: String
 ) {
-    LIGHT({}, "/style/shared/shared.css", "/style/light/light.css"),
-    DARK({}, "/style/shared/shared.css", "/style/dark/dark.css");
+    LIGHT({}, "/style/default/shared.css", "/style/default/light.css"),
+    DARK({}, "/style/default/shared.css", "/style/default/dark.css");
 
     fun invokeHook() = hook.invoke()
 }
