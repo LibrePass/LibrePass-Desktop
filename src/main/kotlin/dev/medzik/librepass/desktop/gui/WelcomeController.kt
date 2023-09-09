7e5d1c643d6eee4af54a961fa454ccea5226ba71
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-09-09 23:44:36 +0200

    fix: removed Initializable interface

--- src/main/kotlin/dev/medzik/librepass/desktop/gui/WelcomeController.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/gui/WelcomeController.kt
@@ -9,14 +9,12 @@ import dev.medzik.librepass.desktop.style.Style
 import dev.medzik.librepass.desktop.style.StyleManager
 import javafx.animation.FadeTransition
 import javafx.fxml.FXML
-import javafx.fxml.Initializable
 import javafx.scene.image.ImageView
 import javafx.util.Duration
 import org.controlsfx.control.ToggleSwitch
-import java.net.URL
 import java.util.*
 
-class WelcomeController : Controller(), Initializable {
+class WelcomeController : Controller() {
     @FXML
     private lateinit var logo: ImageView
 
@@ -26,7 +24,7 @@ class WelcomeController : Controller(), Initializable {
     private lateinit var themeSwitch: ToggleSwitch
 
     @FXML
-    override fun initialize(location: URL, resources: ResourceBundle) {
+    private fun initialize() {
         fadeInTransition = FadeTransition(Duration.seconds(1.6), logo).apply {
             fromValue = 0.0
             toValue = 1.0
@@ -43,7 +41,7 @@ class WelcomeController : Controller(), Initializable {
             Config.overrideObject<Settings>("settings") { theme -> theme.copy(theme = style) }
 
             StyleManager.setStyle(style)
-            themeSwitch.text = if (themeSwitch.isSelected) resources.getString("dark") else resources.getString("light")
+            themeSwitch.text = if (themeSwitch.isSelected) tr("dark") else tr("light")
             StyleManager.reloadStyle()
         }
     }
