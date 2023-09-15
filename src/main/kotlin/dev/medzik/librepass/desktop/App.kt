82f897d057a4106aac92d46c1358b6602a1dd28f
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-09-15 21:54:25 +0200

    style: little changes

--- src/main/kotlin/dev/medzik/librepass/desktop/App.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/App.kt
@@ -13,7 +13,6 @@ import javafx.scene.Scene
 import javafx.scene.image.Image
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
-import java.util.*
 
 class App : Application() {
     companion object {
