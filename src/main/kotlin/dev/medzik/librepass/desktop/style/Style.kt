8f990d3f9325aade798862723ff0743144444d83
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-10-01 22:27:00 +0200

    fix: removed unused methods

--- src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
@@ -1,11 +1,8 @@
 package dev.medzik.librepass.desktop.style
 
 enum class Style(
-    val hook: () -> Unit,
     vararg val styles: String
 ) {
-    LIGHT({}, "/style/shared.css", "/style/light.css"),
-    DARK({}, "/style/shared.css", "/style/dark.css");
-
-    fun invokeHook() = hook.invoke()
+    LIGHT("/style/shared.css", "/style/light.css"),
+    DARK("/style/shared.css", "/style/dark.css")
 }
