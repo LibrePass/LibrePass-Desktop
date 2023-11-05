9468bd819c7d70d49e4ff40e291c888fcb1312e9
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-11-05 23:05:16 +0100

    feat: moved data classes to Objects.kt

--- src/main/kotlin/dev/medzik/librepass/desktop/config/Config.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/config/Config.kt
@@ -86,30 +86,4 @@ object Config {
     }
 }
 
-data class Credentials(
-    val userId: UUID,
-    val email: String,
-    val apiUrl: String? = null,
-    val apiKey: String,
-    val publicKey: String,
-    val lastSync: Long? = null,
-    // argon2id parameters
-    val memory: Int,
-    val iterations: Int,
-    val parallelism: Int,
-)
 
-data class UserSecrets(
-    val privateKey: String,
-    val secretKey: String
-)
-
-data class Settings(
-    var theme: Style
-)
-
-data class StoreCipher(
-    val id: UUID,
-    val owner: UUID,
-    var encryptedCipher: EncryptedCipher
-)
