daf07853a149f2323305f631f9d2b0187f23be31
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-11-22 20:45:13 +0100

    feat: splitted ui into components

--- src/main/kotlin/dev/medzik/librepass/desktop/gui/components/cipher/CipherSection.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/gui/components/cipher/CipherSection.kt
@@ -1,12 +1,34 @@
 package dev.medzik.librepass.desktop.gui.components.cipher
 
 import dev.medzik.librepass.desktop.utils.Fxml
+import javafx.fxml.FXML
+import javafx.scene.control.Label
 import javafx.scene.layout.VBox
 
-class CipherSection(
+open class CipherSection(
     val name: String
 ) : VBox() {
+
+    @FXML
+    private lateinit var nameLabel: Label
+
+    @FXML
+    private lateinit var entriesPane: VBox
+
     init {
         Fxml.loadComponent("/fxml/dashboard/cipher/ciphersection.fxml", this, this)
+    }
+
+    @FXML
+    fun initialize() {
+        nameLabel.text = name;
+    }
+
+    fun clearAllEntries() {
+        entriesPane.children.clear()
+    }
+
+    fun addEntry(entry: CipherEntry) {
+        entriesPane.children.add(entry)
     }
 }
---
+++ src/main/kotlin/dev/medzik/librepass/desktop/gui/components/cipher/CopyCipherEntry.kt
@@ -0,0 +1,38 @@
+package dev.medzik.librepass.desktop.gui.components.cipher
+
+import javafx.scene.control.Button
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.scene.input.Clipboard
+import javafx.scene.input.ClipboardContent
+
+open class CopyCipherEntry(name: String) : CipherEntry(name) {
+
+    protected val copyButton: Button = Button().apply {
+        setOnAction {
+            copy(value.text)
+        }
+    }
+
+    init {
+        copyButton.apply {
+            setPrefSize(45.0,45.0)
+            setMinSize(45.0,45.0)
+            setMaxSize(45.0,45.0)
+        }
+        copyButton.graphic = ImageView(Image(CopyCipherEntry::class.java.getResourceAsStream("/img/dashboard/copy.png"))).apply {
+            isPreserveRatio = true
+            fitWidth = 48.0
+            fitHeight = 28.0
+        }
+
+        buttonsPane.children.add(copyButton)
+    }
+
+    protected fun copy(text: String) {
+        val clipboard = Clipboard.getSystemClipboard()
+        val content = ClipboardContent()
+        content.putString(text)
+        clipboard.setContent(content)
+    }
+}
