7a14afc81f48b01308bccfe8cbbfe2db572f4284
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-11-30 21:52:58 +0100

    feat: added more organized theme stylesheets

--- src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
+++ src/main/kotlin/dev/medzik/librepass/desktop/style/Style.kt
@@ -7,6 +7,6 @@ enum class Style(
     val onSet: (Scene) -> Unit,
     vararg val styles: String
 ) {
-    LIGHT({ it.fill = Color.web("#f5f5f5") },"/style/shared.css", "/style/light.css"),
-    DARK({ it.fill = Color.web("#1e1e1e") },"/style/shared.css", "/style/dark.css")
+    LIGHT({ it.fill = Color.web("#f5f5f5") },"/style/colors-light.css", "/style/controls.css","/style/custom.css"),
+    DARK({ it.fill = Color.web("#1e1e1e") },"/style/controls.css","/style/colors-dark.css","/style/custom.css")
 }
